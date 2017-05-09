4adefe87f43e4d7560060c915fcdcb41c31b7942
MYHOME\Ran <806869597@qq.com>
2017-05-09 17:23:33 +0800

    做到第三屏的动画 动画绑在window的onmouseover上 滑动到指定页面时加载 再判断是否存在 存在则不加载

--- js/index.js
+++ js/index.js
@@ -1,10 +1,53 @@
+// var item_one_layout = document.getElementById('screen_item_one');
+// var item_two_layout = document.getElementById('screen_item_two');
+// var item_three_layout = document.getElementById("screen_item_three");
+// var item_four_layout = document.getElementById("screen_item_four");
+// var item_five_layout = document.getElementById("screen_item_five");
+var layoutArray = ['#screen_item_one','#screen_item_two','#screen_item_three','#screen_item_four','#screen_item_five'];
 
-$(window).scroll(function () {
-  var scrollHeight = $(window).scrollTop();
-  if(typeof(scrollHeight) == "number"){
-    console.log(scrollHeight);
-  }
-  if ($(window).scrollTop() == $(document).height() - $(window).height()) {
-    alert('bottom!!');
-  }
+$('window').ready(function(){
+  $('#screen_item_one').mouseover(function(){
+      var flag = $("#screen_item_one").hasClass("done");
+      setScreenOneAnimation(flag,"#screen_item_one");
+  });
+  $('#screen_item_two').mouseover(function(){
+      var flag = $("#screen_item_two").hasClass("done");
+      setScreenTwoAnimation(flag,"#screen_item_two");
+  });
+
+  $('#screen_item_three').mouseover(function(){
+    var flag = $("#screen_item_three").hasClass("done");
+    setScreenThreeAnimation(flag,"#screen_item_three");
+  });
 });
+
+function setScreenOneAnimation(flag,view){
+  if(!flag){
+    $("#screen_item_one_phone").addClass("item_one_phone_done");
+    $("#screen_item_one_title").addClass("item_one_title_done");
+    $("#screen_item_one_shadow").addClass("item_one_shadow_done");
+
+    $(view).remove("init");
+    $(view).addClass("done");
+  }
+}
+
+function setScreenTwoAnimation(flag,view){
+  if(!flag){
+    $("#screen_item_two_title").addClass("item_two_title_done");
+    $("#screen_item_two_phone").addClass("item_two_phone_done");
+
+    $(view).remove("init");
+    $(view).addadd("done");
+  }
+}
+
+function setScreenThreeAnimation(flag,view){
+  if(!flag){
+    $("#screen_item_two_title").addClass("item_two_title_done");
+    $("#screen_item_two_phone").addClass("item_three_");
+
+    $(view).remove("init");
+    $(view).addadd("done");
+  }
+}
